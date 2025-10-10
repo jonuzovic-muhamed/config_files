e5ca46ae36cccf2ea30d7986bed51f98f895f652
Muhamed Jonuzovic <jon.muha@mailbox.org>
2025-10-10 21:31:12 +0200

    Remove redundant waybar directory

--- sway_wm_setup/waybar/waybar/scripts/check_updates.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-status=$(dnf check-update --quiet | wc -l)
-
-if [ $status -gt 0 ]; then
-  echo ""
-fi
--- sway_wm_setup/waybar/waybar/scripts/gpu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Get GPU temp (example for AMD GPU — adjust if using NVIDIA)
-gpu_temp_raw=$(cat /sys/class/hwmon/hwmon2/temp1_input 2>/dev/null)
-gpu_temp=$(awk "BEGIN {printf \"%.1f\", $gpu_temp_raw/1000}")
-
-gpu_usage=$(radeontop -d - -l 1 | grep gpu | cut -d ' ' -f 5 | cut -c 1-5)
-
-echo "GPU ${gpu_usage} ${gpu_temp}°C"
