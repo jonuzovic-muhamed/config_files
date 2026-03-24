384679e0b60fb8434f9778554c0b0219e929d2fe
Muhamed Jonuzovic <jon.muha@mailbox.org>
2026-03-24 19:38:08 +0100

    Add Homebrew shellenv eval to bash tools
    Enable Homebrew environment in bash by evaluating `brew shellenv` in bash/.bash/tools.sh.

--- bash/.bash/tools.sh
+++ bash/.bash/tools.sh
@@ -33,3 +33,6 @@ fi
 if [[ -s "$NVM_DIR/bash_completion" ]]; then
   source "$NVM_DIR/bash_completion"
 fi
+
+# ------- HomeBrew Package Manager Completion --------
+eval "$(/opt/homebrew/bin/brew shellenv bash)"
