488c266032ef70cef9a7b6840e33ef920ec5f9d3
Muhamed Jonuzovic <jon.muha@mailbox.org>
2026-03-24 21:52:48 +0100

    Suppress macOS Zsh deprecation warning
    Suppress the macOS message about deprecating Bash in favor of Zsh.

--- bash/.bash/env.sh
+++ bash/.bash/env.sh
@@ -16,3 +16,6 @@ export LIBVIRT_DEFAULT_URI='qemu:///system'
 
 # Colors for ls
 export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43:'
+
+# MacOS Silence Apple force of ZSH
+export BASH_SILENCE_DEPRECATION_WARNING=1
